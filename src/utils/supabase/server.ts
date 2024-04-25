9e10373a17d1efd45141ab78107c0c694ee3e0d9
Chrissy Hunt <chrissy.hunt@gmail.com>
2024-04-25 18:33:54 -0400

    add database types

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/database.types';
 
 // only for server-side use to retrieve content
 // TODO: replace with recommended Supabase client code
 // once auth is introduced - see https://supabase.com/docs/guides/getting-started/tutorials/with-nextjs
 export function createServiceClient() {
-  return createClient(
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_KEY!
   );
