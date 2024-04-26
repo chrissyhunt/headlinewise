92a7c00467af0c2c7dd047da118848c47520e879
Chrissy Hunt <chrissy.hunt@gmail.com>
2024-04-26 11:50:49 -0400

    save news articles to supabase

--- src/app/get-news/route.ts
+++ src/app/get-news/route.ts
@@ -6,26 +6,60 @@ export async function GET(request: Request) {
   const supabase = createServiceClient();
 
   // get list of topics
-  const { data, error } = await supabase.from('topics').select();
-  const topics = data?.map(t => t.query);
+  const { data: topics, error } = await supabase.from('topics').select();
 
   if (error || !topics) {
     return new Response('Error retrieving topics', {
       status: 500
-    })
+    });
   }
 
   const results = [];
 
-  for (let topic of topics) {
-    const sources = makeSourceBatches();
-    for (let batch of sources) {
-      const data = await fetchNews(topic!, batch);
-      // filter out 'removed' results with null source ids returned by API
-      results.push(...data.articles.filter((a: any) => a.source?.id));
+  // get articles from NewsAPI
+  try {
+    for (let topic of topics) {
+      const sources = await makeSourceBatches();
+      for (let batch of sources) {
+        const data = await fetchNews(topic.query!, batch);
+        // filter out 'removed' results with null source ids returned by API
+        // format as expected by Supabase
+        results.push(
+          ...data.articles
+          .filter((a: any) => a.source?.id)
+          .map((a: any) => ({
+            title: a.title,
+            description: a.description,
+            author: a.author,
+            url: a.url,
+            url_to_image: a.urlToImage,
+            content: a.content,
+            published_at: a.publishedAt,
+            source: a.source.id,
+            topic: topic.id,
+          }))
+        );
+      }
     }
+  } catch (e) {
+    return new Response('Error querying NewsAPI', {
+      status: 500
+    });
   }
 
-  console.log('RESULTS: ', results.length);
-  return Response.json(results);
+  // save to Supabase
+  if (results.length) {
+    try {
+      const { error } = await supabase.from('articles').insert(results);
+      if (error) throw new Error();
+    } catch (e) {
+      return new Response('Error saving news articles', {
+        status: 500
+      });
+    }
+  }
+  
+  return new Response('Success!', {
+    status: 200
+  });
 }
--- src/utils/news-api/sources.ts
+++ src/utils/news-api/sources.ts
@@ -1,46 +1,12 @@
 import chunk from 'lodash.chunk';
-
-const sources = [
-  'wired',
-  'vice-news',
-  'usa-today',
-  'time',
-  'the-washington-times',
-  'the-washington-post',
-  'the-wall-street-journal',
-  'the-verge',
-  'the-lad-bible',
-  'the-huffington-post',
-  'the-hill',
-  'the-american-conservative',
-  'techradar',
-  'techcrunch',
-  'reuters',
-  'reddit-r-all',
-  'politico',
-  'new-york-magazine',
-  'newsweek',
-  'nbc-news',
-  'national-review',
-  'msnbc',
-  'independent',
-  'google-news',
-  'fox-news',
-  'financial-post',
-  'cnn',
-  'cbs-news',
-  'buzzfeed',
-  'business-insider',
-  'breitbart-news',
-  'bloomberg',
-  'bbc-news',
-  'axios',
-  'associated press',
-  'al-jazeera-english',
-  'abc-news'
-];
+import { createServiceClient } from "@/utils/supabase/server";
 
 // limit set by News Api
 const MAX_SOURCES_PER_REQUEST = 20;
 
-export const makeSourceBatches = () => chunk(sources, MAX_SOURCES_PER_REQUEST);
+export const makeSourceBatches = async () => {
+  const supabase = await createServiceClient();
+  const results = await supabase.from('sources').select();
+  const sources = results.data?.map(s => s.id);
+  return chunk(sources, MAX_SOURCES_PER_REQUEST);
+}
