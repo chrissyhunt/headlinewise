ac3f03f0a0ea3327f8ec37d9c3a8db2023409a52
Chrissy Hunt <chrissy.hunt@gmail.com>
2024-04-29 17:06:33 -0400

    fix route

--- src/app/api/cron-test/route.ts
+++ src/app/api/cron-test/route.ts
@@ -1,6 +1,6 @@
 export async function GET(request: Request) {
   const authHeader = request.headers.get("Authorization");
-  if (authHeader !== `Bearer ${process.env.ENDPOINT_TOKEN}`) {
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", {
       status: 401,
     });
