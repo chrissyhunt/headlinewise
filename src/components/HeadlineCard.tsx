12f194b8db2777c91022808f747d418461956571
Chrissy Hunt <chrissy.hunt@gmail.com>
2024-04-30 13:53:34 -0400

    fix headline card height

--- src/components/HeadlineCard.tsx
+++ src/components/HeadlineCard.tsx
@@ -5,7 +5,7 @@ interface HeadlineCardProps {
 
 export default function HeadlineCard({ title, date }: HeadlineCardProps) {
   return (
-    <div className="text-2xl text-left p-8 pb-16 border-b-4 border-b-transparent hover:bg-fuchsia-50 hover:border-b-4 hover:border-b-fuchsia-400 hover:shadow-sm">
+    <div className="h-full text-2xl text-left p-8 pb-16 border-b-4 border-b-transparent hover:bg-fuchsia-50 hover:border-b-4 hover:border-b-fuchsia-400 hover:shadow-sm">
       <span className="font-serif">{title}</span>
       <br />
       <span className="font-sans text-sm">{new Date(date).toDateString()}</span>
