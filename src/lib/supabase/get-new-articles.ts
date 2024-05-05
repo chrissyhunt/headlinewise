7c6f332b4d8df7df6c02419e321d6c4826705bfc
Chrissy Hunt <chrissy.hunt@gmail.com>
2024-05-05 08:01:38 -0400

    limit new articles

--- src/lib/supabase/get-new-articles.ts
+++ src/lib/supabase/get-new-articles.ts
@@ -4,7 +4,8 @@ export const getNewArticles = async () => {
   const supabase = createServiceClient();
   const { data, error } = await supabase
     .from("articles")
-    .select("url,title,description,analysis(id)");
+    .select("url,title,description,analysis(id)")
+    .limit(60);
 
   if (error) throw new Error("Error getting articles", { cause: error });
   const articlesWithoutAnalysis = data?.filter((a) => a.analysis.length < 1);
