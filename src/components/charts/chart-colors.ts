d5d2940a1adf0b1829deea2b82fbd872917ece92
Chrissy Hunt <chrissy.hunt@gmail.com>
2024-05-10 12:06:37 -0400

    add more bar colors

--- src/components/charts/chart-colors.ts
+++ src/components/charts/chart-colors.ts
@@ -4,6 +4,9 @@ export const chartColors = [
   colors["cyan"]["600"],
   colors["fuchsia"]["600"],
   colors["violet"]["600"],
+  colors["indigo"]["600"],
+  colors["pink"]["600"],
+  colors["sky"]["600"],
 ];
 
 export const lineColor = colors["fuchsia"]["400"];
