e75e03e9aa04044de7ff6ccce164af2b905457ad
Chrissy Hunt <30362010+chrissyhunt@users.noreply.github.com>
2024-05-20 10:40:44 -0400

    Cleanup (#56)
    * update models, order get new articles results
    
    * revalidate data page once per day
    
    * add more metadata to head

---
+++ src/lib/constants.ts
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+
+export const defaultMetadata: Metadata = {
+  title: {
+    template: "%s | HeadlineWise",
+    default: "HeadlineWise",
+  },
+  description: "A headline analysis tool built by Chrissy Hunt",
+  applicationName: "HeadlineWise",
+  authors: {
+    url: "https://chrissyhunt.com",
+    name: "Chrissy Hunt",
+  },
+  keywords: ["media literacy", "news", "headlines", "ai analysis", "claude ai"],
+  robots: {
+    index: true,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    type: "website",
+    url: "https://www.headlinewise.com",
+    title: "HeadlineWise",
+    description: "A headline analysis tool built by Chrissy Hunt",
+    siteName: "HeadlineWise",
+    images: {
+      url: "https://www.headlinewise.com/images/headlinewise-screenshot.png",
+      alt: "Screenshot of headlinewise.com",
+      width: 1280,
+      height: 800,
+    },
+  },
+};
